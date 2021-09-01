a33dcdebff96c840393bdebd6b612ac4357d46ac
brunoplasdan <bruno.sousa@plasdan.pt>
2021-09-01 18:01:11 +0100

    multer configuration

--- routes/books.js
+++ routes/books.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const router = express();
+const multer = require('multer');
+const path = require('path');
 const Book = require('../models/book');
+const uploadPath = path.join('public', Book.coverImageBasePath)
+const imageMimeTypes = ['image/jpeg','image/png','image/gif']
 const Author = require('../models/author');
-
+const upload = multer({
+    dest: uploadPath,
+    fileFilter: (req, file, callback) => {
+        callback(null, )
+    }
+});
 
 // All Books Route
 router.get('/', async (req, res) => {
@@ -24,14 +33,22 @@ router.get('/new', async (req, res) => {
 });
 
 // Create Book route
-router.post('/', async(req, res) => {
+router.post('/', upload.single('cover'), async (req, res) => {
+    req.fileName != null ? req.file.filename : null
     const book = new Book({
         title: req.body.title,
         author: req.body.author,
         publishDate: new Date(req.body.publishDate),
         pageCount: req.body.pageCount,
+        coverImageName: fileName,
         description: req.body.description
     });
+    try {
+        const newBook = await book.save();
+        res.redirect('books');
+    } catch {
+        
+    }
 });
 
 module.exports = router;
