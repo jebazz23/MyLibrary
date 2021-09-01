90f729bbda0422fcfad706c7e47ee0e7e1861fc3
brunoplasdan <bruno.sousa@plasdan.pt>
2021-09-01 12:55:12 +0100

    Initial Author Routes

--- server.js
+++ server.js
@@ -6,7 +6,14 @@ const express = require('express');
 const app = express();
 const expressLayouts = require('express-ejs-layouts');
 
+// parse request data content type application/x-www-form-urlencoded
+app.use(express.urlencoded({ limit:'10mb', extended: false }));
+
+//parse request data content type application/json
+app.use(express.json());
+
 const indexRouter = require('./routes/index');
+const authorRouter = require('./routes/authors');
 
 app.set('view engine', 'ejs');
 app.set('views', __dirname + '/views');
@@ -25,5 +32,6 @@ db.on('error', error => console.error(error))
 db.once('once', () => console.log('Connected to Mongoose'));
 
 app.use('/',indexRouter);
+app.use('/authors', authorRouter);
 
 app.listen(process.env.PORT || 3000);
